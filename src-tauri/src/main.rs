6b75102364532de0eafb377264f814a66f587e57
Roy Sun <roy.sun@sunrecruitment.co.nz>
2022-11-08 01:14:12 +1300

    complete functionality and restruture dist folder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,14 @@
     windows_subsystem = "windows"
 )]
 
+use std::{sync::{Arc, Mutex}, time::Duration};
+
+use tauri::{State, Manager};
+use tokio::time::sleep;
+
+#[derive(Default)]
+struct Counter(Arc<Mutex<i32>>);
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,8 +19,34 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app|{
+          let app_handle = app.handle();
+          tauri::async_runtime::spawn(async move {
+            loop {
+                sleep(Duration::from_secs(2)).await;
+                println!("Sending backend-ping");
+                app_handle.emit_all("backend-ping","ping").unwrap();
+            }
+          });  
+
+          Ok(())
+        })
+        .manage(Counter(Default::default()))
+        .invoke_handler(tauri::generate_handler![hello_world, add_count])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+
+#[tauri::command]
+fn hello_world() -> String {
+    "Hello World - time to sleep!".to_string()
+}
+
+#[tauri::command]
+fn add_count(num: i32, counter: State<'_, Counter>) -> String {
+    let mut val = counter.0.lock().unwrap();
+    *val += num;
+    format!("{val}")
 }
 
---
+++ src-ui/src/main.ts
@@ -0,0 +1,37 @@
+import { invoke } from "@tauri-apps/api";
+import { listen, Event as TauriEvent } from "@tauri-apps/api/event";
+
+const $ = document.querySelector.bind(document);
+
+document.addEventListener("DOMContentLoaded", async function() {
+    // get the elements
+    const helloEl = $("div.hello")! as HTMLElement;
+
+    let counterButtonEl = $("counter-button")! as HTMLElement;
+    let counterResultEl = $("counter-result")! as HTMLElement;
+
+    let pingEl = $("backend-ping")! as HTMLElement;
+
+    // listen backend-ping event (from Tauri Rust App)
+    listen("backend-ping", function (evt: TauriEvent<any>)  {
+        pingEl.classList.add("on");
+        setTimeout(function() {
+            pingEl.classList.remove("on");
+        }, 500);
+    });
+
+    // counter button click
+    counterButtonEl.addEventListener("pointerup", async function() {
+        const result = await invoke("add_count", { num: 1 }) as string;
+        counterResultEl.textContent = result;
+    });
+
+    // hello click
+    helloEl.addEventListener("pointerup", async function() {
+        const result = await invoke("hello_world") as string;
+        helloEl.textContent = result;
+        setTimeout(function(){
+            helloEl.textContent = "Click again!";
+        }, 1000);
+    });
+});
